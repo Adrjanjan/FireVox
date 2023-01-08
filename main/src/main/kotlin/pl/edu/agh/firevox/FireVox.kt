a57f216618e24d05e406d259b31a36ebe2cc9cdc
Adrian Janakiewicz <adrjanjan@gmail.com>
2023-01-08 04:28:02 +0100

    Finally fixed xD

--- main/src/main/kotlin/pl/edu/agh/firevox/FireVox.kt
+++ main/src/main/kotlin/pl/edu/agh/firevox/FireVox.kt
@@ -1,15 +1,14 @@
 package pl.edu.agh.firevox
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.springframework.integration.config.EnableIntegration
-
 
 @SpringBootApplication
-@EnableJpaRepositories
-//@EnableIntegration
+@EntityScan("pl.edu.agh.firevox")
+@EnableJpaRepositories(basePackages = ["pl.edu.agh.firevox"])
 @ComponentScan(
     basePackages = ["pl.edu.agh.firevox.*", "pl.edu.agh.firevox.shared.*"]
 )
--- worker/src/main/kotlin/pl/edu/agh/firevox/worker/WorkerApplication.kt
+++ worker/src/main/kotlin/pl/edu/agh/firevox/worker/WorkerApplication.kt
@@ -1,15 +1,16 @@
 package pl.edu.agh.firevox.worker
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
-
 @SpringBootApplication
-@EnableJpaRepositories
+@EntityScan("pl.edu.agh.firevox")
+@EnableJpaRepositories(basePackages = ["pl.edu.agh.firevox"])
 @ComponentScan(
-    basePackages = ["pl.edu.agh.firevox.*", "pl.edu.agh.firevox.shared.*"]
+    basePackages = ["pl.edu.agh.firevox", "pl.edu.agh.firevox.shared"]
 )
 class WorkerApplication
 
