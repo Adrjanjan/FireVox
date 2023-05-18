37bc86c07efb9980515b3784580b0314a59a0d21
Adrian Janakiewicz <adrjanjan@gmail.com>
2023-05-19 00:21:02 +0200

    transitions init

--- worker/src/main/kotlin/pl/edu/agh/firevox/worker/physics/PhysicsCalculator.kt
+++ worker/src/main/kotlin/pl/edu/agh/firevox/worker/physics/PhysicsCalculator.kt
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service
 import pl.edu.agh.firevox.shared.model.CustomVoxelRepository
 import pl.edu.agh.firevox.shared.model.StateProperties
 import pl.edu.agh.firevox.shared.model.Voxel
+import pl.edu.agh.firevox.shared.model.VoxelMaterial
 import pl.edu.agh.firevox.shared.model.VoxelMaterial.*
 
 @Service
@@ -13,32 +14,38 @@ class PhysicsCalculator(
 
     fun calculate(voxel: Voxel) {
         val newMaterial = when (voxel.currentProperties.material) {
-            AIR -> TODO()
-            HALF_SMOKE -> TODO()
-            FULL_SMOKE -> TODO()
-            WOOD -> TODO()
-            WOOD_HEATED -> TODO()
-            WOOD_BURNING -> TODO()
-            WOOD_BURNT -> TODO()
-            PLASTIC -> TODO()
-            PLASTIC_BURNING -> TODO()
-            PLASTIC_BURNT -> TODO()
-            TEXTILE -> TODO()
-            TEXTILE_BURNING -> TODO()
-            TEXTILE_BURNT -> TODO()
-            METAL -> TODO()
-            METAL_HEATED -> TODO()
-            METAL_HOT -> TODO()
-            METAL_VERY_HOT -> TODO()
-            GLASS -> TODO()
-            GLASS_HEATED -> TODO()
-            GLASS_HOT -> TODO()
-            GLASS_VERY_HOT -> TODO()
-            CONCRETE -> TODO()
-            FLAME -> TODO()
+            AIR, HALF_SMOKE, FULL_SMOKE, FLAME
+            -> airTransitions(voxel)
+
+            WOOD, WOOD_HEATED, WOOD_BURNING, WOOD_BURNT, PLASTIC, PLASTIC_BURNING,
+            PLASTIC_BURNT, TEXTILE, TEXTILE_BURNING, TEXTILE_BURNT
+            -> burningTransitions(voxel)
+
+            METAL, METAL_HEATED, METAL_HOT, METAL_VERY_HOT, GLASS, GLASS_HEATED, GLASS_HOT, GLASS_VERY_HOT
+            -> conductionTransitions(voxel)
+
+            CONCRETE
+            -> concreteTransitions(voxel)
+
         }
         val newIteration = voxel.currentProperties.iterationNumber + 1
         voxel.nextProperties = StateProperties(newIteration, newMaterial)
+    }
+
+    private fun concreteTransitions(voxel: Voxel): VoxelMaterial {
+        TODO("Not yet implemented")
+    }
+
+    private fun conductionTransitions(voxel: Voxel): VoxelMaterial {
+        TODO("Not yet implemented")
+    }
+
+    private fun burningTransitions(voxel: Voxel): VoxelMaterial {
+        TODO("Not yet implemented")
+    }
+
+    private fun airTransitions(voxel: Voxel): VoxelMaterial {
+        TODO("Not yet implemented")
     }
 
     fun Voxel.isSolid() = this.currentProperties.material in listOf(
