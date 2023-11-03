ee3fc59613f7c3b69f9b2e3be5078280f8bf2e53
Adrian Janakiewicz <adrjanjan@gmail.com>
2023-11-03 05:45:38 +0100

    base materials proposition

--- shared/src/main/kotlin/pl/edu/agh/firevox/shared/model/PhysicalMaterial.kt
+++ shared/src/main/kotlin/pl/edu/agh/firevox/shared/model/PhysicalMaterial.kt
@@ -32,7 +32,8 @@ class PhysicalMaterial(
     val smokeEmissionPerSecond: Double?, // no unit
     val deformationTemperature: Double?, // unit K
 
-    val burnsCompletely: Boolean = false
+    val burnsCompletely: Boolean = false,
+    val emissivity: Double = 0.0,
 ) {
 
     @Id
@@ -115,8 +116,163 @@ class MaterialsConfig(
             density = 1.204,
             baseTemperature = 20.0.toKelvin(),
             thermalConductivityCoefficient = 25.87,
+            convectionHeatTransferCoefficient = 38.0,
+            specificHeatCapacity = 1015.0,
+            ignitionTemperature = null,
+            timeToIgnition = null,
+            autoignitionTemperature = null,
+            burningTime = null,
+            effectiveHeatOfCombustion = null,
+            smokeEmissionPerSecond = null,
+            deformationTemperature = null
+        ).let(materials::add)
+
+        PhysicalMaterial(
+            voxelMaterial = VoxelMaterial.SMOKE,
+            density = 1.4,
+            baseTemperature = 40.0.toKelvin(),
+            thermalConductivityCoefficient = 25.87,
+            convectionHeatTransferCoefficient = 38.0,
+            specificHeatCapacity = 1015.0,
+            ignitionTemperature = null,
+            timeToIgnition = null,
+            autoignitionTemperature = null,
+            burningTime = null,
+            effectiveHeatOfCombustion = null,
+            smokeEmissionPerSecond = null,
+            deformationTemperature = null
+        ).let(materials::add)
+
+        PhysicalMaterial(
+            voxelMaterial = VoxelMaterial.FLAME,
+            density = 0.204,
+            baseTemperature = 150.0.toKelvin(),
+            thermalConductivityCoefficient = 25.87,
+            convectionHeatTransferCoefficient = 38.0,
+            specificHeatCapacity = 1015.0,
+            ignitionTemperature = null,
+            timeToIgnition = null,
+            autoignitionTemperature = null,
+            burningTime = null,
+            effectiveHeatOfCombustion = null,
+            smokeEmissionPerSecond = null,
+            deformationTemperature = null
+        ).let(materials::add)
+
+        listOf(VoxelMaterial.WOOD, VoxelMaterial.WOOD_BURNING, VoxelMaterial.WOOD_BURNT).map {
+            PhysicalMaterial(
+                voxelMaterial = it,
+                density = 400.0,
+                baseTemperature = 20.0.toKelvin(),
+                thermalConductivityCoefficient = 0.3,
+                convectionHeatTransferCoefficient = 0.0,
+                specificHeatCapacity = 2390.0,
+                ignitionTemperature = 250.toKelvin(),
+                timeToIgnition = 1.0, // TODO
+                autoignitionTemperature = 2000.toKelvin(),
+                burningTime = 1.4, // TODO
+                effectiveHeatOfCombustion = 1500.0,
+                smokeEmissionPerSecond = 0.3,
+                deformationTemperature = null,
+                emissivity = 0.9
+            )
+        }.let(materials::addAll)
+
+        listOf(VoxelMaterial.PLASTIC, VoxelMaterial.PLASTIC_BURNING, VoxelMaterial.PLASTIC_BURNT).map {
+            PhysicalMaterial(
+                voxelMaterial = it,
+                density = 300.0,
+                baseTemperature = 20.0.toKelvin(),
+                thermalConductivityCoefficient = 0.174,
+                convectionHeatTransferCoefficient = 0.0,
+                specificHeatCapacity = 5200.0,
+                ignitionTemperature = 120.toKelvin(),
+                timeToIgnition = 0.4, // TODO
+                autoignitionTemperature = 750.toKelvin(),
+                burningTime = 0.6, // TODO
+                effectiveHeatOfCombustion = 500.0,
+                smokeEmissionPerSecond = 0.8,
+                deformationTemperature = null,
+                emissivity = 0.9
+            )
+        }.let(materials::addAll)
+
+        listOf(VoxelMaterial.TEXTILE, VoxelMaterial.TEXTILE_BURNING, VoxelMaterial.TEXTILE_BURNT).map {
+            PhysicalMaterial(
+                voxelMaterial = it,
+                density = 400.0,
+                baseTemperature = 20.0.toKelvin(),
+                thermalConductivityCoefficient = 0.3,
+                convectionHeatTransferCoefficient = 0.0,
+                specificHeatCapacity = 2390.0,
+                ignitionTemperature = 350.toKelvin(),
+                timeToIgnition = 0.2, // TODO
+                autoignitionTemperature = 1400.toKelvin(),
+                burningTime = 0.4, // TODO
+                effectiveHeatOfCombustion = 1500.0,
+                smokeEmissionPerSecond = 1.0,
+                deformationTemperature = null
+            )
+        }.let(materials::addAll)
+
+        PhysicalMaterial(
+            voxelMaterial = VoxelMaterial.METAL,
+            density = 2700.0,
+            baseTemperature = 20.toKelvin(),
+            thermalConductivityCoefficient = 235.0,
             convectionHeatTransferCoefficient = 0.0,
-            specificHeatCapacity = 1.0061,
+            specificHeatCapacity = 897.0,
+            ignitionTemperature = null,
+            burningTime = null,
+            timeToIgnition = null,
+            autoignitionTemperature = null,
+            effectiveHeatOfCombustion = null,
+            smokeEmissionPerSecond = null,
+            deformationTemperature = null,
+            emissivity = 0.1,
+        ).let(materials::add)
+
+        PhysicalMaterial(
+            voxelMaterial = VoxelMaterial.GLASS,
+            density = 2700.0,
+            baseTemperature = 20.0.toKelvin(),
+            thermalConductivityCoefficient = 1.0,
+            convectionHeatTransferCoefficient = 55.0,
+            specificHeatCapacity = 8400.0,
+            ignitionTemperature = null,
+            timeToIgnition = null,
+            autoignitionTemperature = null,
+            burningTime = null,
+            effectiveHeatOfCombustion = null,
+            smokeEmissionPerSecond = null,
+            deformationTemperature = 150.0,
+            emissivity = 0.89
+        ).let(materials::add)
+
+        PhysicalMaterial(
+            voxelMaterial = VoxelMaterial.CONCRETE,
+            density = 2392.0,
+            baseTemperature = 20.toKelvin(),
+            thermalConductivityCoefficient = 2.071,
+            convectionHeatTransferCoefficient = 55.6,
+            specificHeatCapacity = 936.3,
+            ignitionTemperature = null,
+            burningTime = null,
+            timeToIgnition = null,
+            autoignitionTemperature = null,
+            effectiveHeatOfCombustion = null,
+            smokeEmissionPerSecond = null,
+            deformationTemperature = null,
+            emissivity = 0.9,
+        ).let(materials::add)
+
+        PhysicalMaterial(
+            voxelMaterial = VoxelMaterial.WATER,
+            density = 0.999,
+            baseTemperature = 20.0.toKelvin(),
+            thermalConductivityCoefficient = 0.6,
+            convectionHeatTransferCoefficient = 1000.0,
+            specificHeatCapacity = 4190.0,
             ignitionTemperature = null,
             timeToIgnition = null,
             autoignitionTemperature = null,
